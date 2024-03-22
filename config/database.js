b64d1f90ff57d12e9f863cca4f30bc0e30216f87
shaonbd135 <shaonsimt@gmail.com>
2024-03-22 21:01:06 +0600

    server updated

--- config/database.js
+++ config/database.js
@@ -1,11 +1,23 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect(process.env.DB_URl)
-    .then(() => {
-        console.log('Database connected');
-    })
-    .catch((err) => {
-        console.log(err);
-        process.exit(1);
-    })
+// mongoose.connect(process.env.DB_URl)
+//     .then(() => {
+//         console.log('Database connected');
+//     })
+//     .catch((err) => {
+//         console.log(err);
+//         process.exit(1);
+//     })
+
+const uri = process.env.DB_URl;
+
+// Connect to MongoDB
+mongoose.connect(uri)
+  .then(() => {
+    console.log('MongoDB connected successfully');
+    // Your server code or additional setup can go here
+  })
+  .catch(err => {
+    console.error('MongoDB connection error:', err);
+  });
