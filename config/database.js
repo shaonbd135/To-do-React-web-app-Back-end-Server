1bce427baaabb7bc86dc54ed5b915a6b139c05cf
shaonbd135 <shaonsimt@gmail.com>
2024-03-22 21:36:53 +0600

    Project Updated

--- config/database.js
+++ config/database.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
-const uri = process.env.DB_URl;
+
 
 // Connect to MongoDB
-mongoose.connect(`${uri}`, { useUnifiedTopology: true, useNewUrlParser: true })
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.d9xsuxf.mongodb.net/SaidursToDo`)
   .then(() => {
     console.log('MongoDB connected successfully');
     // Your server code or additional setup can go here
