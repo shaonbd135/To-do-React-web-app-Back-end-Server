ce618566bcfec3f5873999b2d7b88f951bf7c1d4
shaonbd135 <shaonsimt@gmail.com>
2024-03-22 21:31:59 +0600

    project Updated

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const uri = process.env.DB_URl;
 
 // Connect to MongoDB
-mongoose.connect(uri)
+mongoose.connect(`${uri}`, { useUnifiedTopology: true, useNewUrlParser: true })
   .then(() => {
     console.log('MongoDB connected successfully');
     // Your server code or additional setup can go here
